46837276fb52c4e1c6af7a4cb26d4cc18cb60e20
Robert Tari <robert@tari.in>
2021-10-18 22:14:06 +0200

    Manage DESKTOP_SESSION in unit tests

--- tests/tst_utils.cpp
+++ tests/tst_utils.cpp
@@ -34,13 +34,17 @@ public:
 
    void TearDown() {
        unsetenv("XDG_CURRENT_DESKTOP");
+       unsetenv("DESKTOP_SESSION");
    }
 };
 
 TEST_F(XdgCurrentDesktopUtilsTest, isLomiri)
 {
     unsetenv("XDG_CURRENT_DESKTOP");
+    unsetenv("DESKTOP_SESSION");
     EXPECT_FALSE(ayatana_common_utils_is_lomiri());
+    setenv("DESKTOP_SESSION", "ubuntu-touch", 1);
+    EXPECT_TRUE(ayatana_common_utils_is_lomiri());
     setenv("XDG_CURRENT_DESKTOP", "Lomiri", 1);
     EXPECT_TRUE(ayatana_common_utils_is_lomiri());
 }
@@ -48,13 +52,19 @@ TEST_F(XdgCurrentDesktopUtilsTest, isLomiri)
 TEST_F(XdgCurrentDesktopUtilsTest, isGnome)
 {
     unsetenv("XDG_CURRENT_DESKTOP");
+    unsetenv("DESKTOP_SESSION");
     EXPECT_FALSE(ayatana_common_utils_is_gnome());
+    setenv("DESKTOP_SESSION", "gnome", 1);
+    EXPECT_TRUE(ayatana_common_utils_is_gnome());
     setenv("XDG_CURRENT_DESKTOP", "GNOME", 1);
     EXPECT_TRUE(ayatana_common_utils_is_gnome());
 }
 TEST_F(XdgCurrentDesktopUtilsTest, isUnity)
 {
     unsetenv("XDG_CURRENT_DESKTOP");
+    unsetenv("DESKTOP_SESSION");
+    EXPECT_FALSE(ayatana_common_utils_is_unity());
+    setenv("DESKTOP_SESSION", "gnome-fallback", 1);
     EXPECT_FALSE(ayatana_common_utils_is_unity());
     setenv("XDG_CURRENT_DESKTOP", "Unity", 1);
     EXPECT_TRUE(ayatana_common_utils_is_unity());
@@ -62,20 +72,27 @@ TEST_F(XdgCurrentDesktopUtilsTest, isUnity)
 TEST_F(XdgCurrentDesktopUtilsTest, isMate)
 {
     unsetenv("XDG_CURRENT_DESKTOP");
+    unsetenv("DESKTOP_SESSION");
     EXPECT_FALSE(ayatana_common_utils_is_mate());
+    setenv("DESKTOP_SESSION", "mate", 1);
+    EXPECT_TRUE(ayatana_common_utils_is_mate());
     setenv("XDG_CURRENT_DESKTOP", "MATE", 1);
     EXPECT_TRUE(ayatana_common_utils_is_mate());
 }
 TEST_F(XdgCurrentDesktopUtilsTest, isXfce)
 {
     unsetenv("XDG_CURRENT_DESKTOP");
+    unsetenv("DESKTOP_SESSION");
     EXPECT_FALSE(ayatana_common_utils_is_xfce());
+    setenv("DESKTOP_SESSION", "xfce", 1);
+    EXPECT_TRUE(ayatana_common_utils_is_xfce());
     setenv("XDG_CURRENT_DESKTOP", "XFCE", 1);
     EXPECT_TRUE(ayatana_common_utils_is_xfce());
 }
 TEST_F(XdgCurrentDesktopUtilsTest, isPantheon)
 {
     unsetenv("XDG_CURRENT_DESKTOP");
+    unsetenv("DESKTOP_SESSION");
     EXPECT_FALSE(ayatana_common_utils_is_pantheon());
     setenv("XDG_CURRENT_DESKTOP", "PANTHEON", 1);
     EXPECT_TRUE(ayatana_common_utils_is_pantheon());
@@ -83,7 +100,10 @@ TEST_F(XdgCurrentDesktopUtilsTest, isPantheon)
 TEST_F(XdgCurrentDesktopUtilsTest, isBudgie)
 {
     unsetenv("XDG_CURRENT_DESKTOP");
+    unsetenv("DESKTOP_SESSION");
     EXPECT_FALSE(ayatana_common_utils_is_budgie());
+    setenv("DESKTOP_SESSION", "budgie-desktop", 1);
+    EXPECT_TRUE(ayatana_common_utils_is_budgie());
     setenv("XDG_CURRENT_DESKTOP", "Budgie:GNOME", 1);
     EXPECT_TRUE(ayatana_common_utils_is_budgie());
 }
